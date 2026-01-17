68b13066de1d85a937a7546554acdd124a17983b
csanadlajko <csanilajko@gmail.com>
2026-01-17 11:04:18 +0100

    Implemented consumer class

--- consume/consumer.py
+++ consume/consumer.py
@@ -1,19 +1,43 @@
 from confluent_kafka import Consumer
 from confluent_kafka.cimpl import Message
+from confluent_kafka import TopicPartition
+from confluent_kafka.admin import _metadata
 from constants.CommonContants import KafkaCommonConstants
 from typing import Any
 
-consumer: Consumer = Consumer(KafkaCommonConstants.CONSUMER_DEFAULT_CONFIG)
+class KafkaConsumer:
 
-def read_topic(topic_name: str, timeout: int = 15) -> dict[str, Any] | None:
-    consumer.subscribe(topics=[topic_name])
-    for _ in range(timeout):
-        msg: Message = consumer.poll(1.0)
-        if msg is None:
-            continue
-        if msg.error():
-            print(f"Failed to read message from topic: {topic_name}\nError: {msg.error()}")
-            return None
+    def __init__(self, topic_name: str):
+        self.topic_name = topic_name
+        self.consumer = Consumer(KafkaCommonConstants.CONSUMER_DEFAULT_CONFIG)
+
+    def read_topic(self, timeout: int = 15) -> dict[str, Any] | None:
+        self.consumer.subscribe(topics=[self.topic_name])
+        for _ in range(timeout):
+            msg: Message = self.consumer.poll(1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                print(f"Failed to read message from topic: {self.topic_name}\nError: {msg.error()}")
+                return None
+            return {
+                "timestamp": msg.timestamp(),
+                "value": msg.value().decode(),
+                "topic_name": msg.topic(),
+                "headers": msg.headers(),
+                "offset": msg.offset(),
+                "partition": msg.partition()
+            }
+
+    def read_latest_message(self) -> dict[str, Any]:
+        topic_list: _metadata.ClusterMetadata = self.consumer.list_topics(self.topic_name, timeout=10)
+        partitions: dict[int, _metadata.PartitionMetadata] = topic_list.topics[self.topic_name].partitions
+        topic_partitions: list[TopicPartition] = [TopicPartition(self.topic_name, p) for p in partitions]
+        for p in topic_partitions:
+            low, high = self.consumer.get_watermark_offsets(p)
+            p.offset = high-1
+        self.consumer.assign(topic_partitions)
+        msg: Message = self.consumer.poll(1.0)
         return {
             "timestamp": msg.timestamp(),
             "value": msg.value().decode(),
--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 from produce.producer import produce_message
-from consume.consumer import read_topic
+from consume.consumer import KafkaConsumer
+
+consumer: KafkaConsumer = KafkaConsumer(
+    topic_name="test-topic"
+)
 
 KAFKA_TOPIC: str = "test-topic"
 PROCUCE_MESSAGE_STRING: str = "a message to consume"
 
 produce_message(KAFKA_TOPIC, PROCUCE_MESSAGE_STRING)
 
-metadata = read_topic(topic_name=KAFKA_TOPIC)
+latest_metadata = consumer.read_latest_message()
 
-print(metadata)
+## build docker for kafka broker
+## sudo docker compose up
